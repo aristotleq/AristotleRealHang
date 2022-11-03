c3309079b4ecee6feacf5782945a0b5358832a68
aristotleq <114704080+aristotleq@users.noreply.github.com>
2022-11-02 23:44:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Loads colorama module #
+from colorama import Fore, Back, Style
+
+
 ### Functions ###
 
 def getLetter():
@@ -19,32 +23,35 @@ def getLetter():
   return letter
   
 
-# Create a function that displays the secret word
-def displaySecretWord(word, guesses):
 
+# Create a function that displays the secret word
+def displaySecretWord(wordReveal, guesses):
+  
   # Go through each letter in the secret word, and determine HOW we display it
-  for letter in word:
+  for letter in wordReveal:
        
     # If this letter (from the secret word) has been guessed, display the letter        
     if(letter in guesses):
       
       print(letter, end=" ")
+     
       
     # Otherwise, display an underscore ( _ )
-    else:
+    else:      
       print("_", end=" ")
       
+                 
       
-
+    
 # Create a function that determines if the user has won the game
-def hasUserWon(word, guesses):
+def hasUserWon(wordReveal, guesses):
  
   # Let's use an "Innocent Until Proven Guilty" Algorithm...
   # ...and create a variable that is set to "True"
   won = True
  
   # Go through each letter in the secret word
-  for letter in word:
+  for letter in wordReveal:
  
     # Check if the letter has been guessed
     if(letter not in guesses):
@@ -59,16 +66,37 @@ def hasUserWon(word, guesses):
 
 ### Main Program ###
 
-word = "LETTER"
+wordReveal = "BETTER"
 letter = ""
 guesses = []
 strikes = 0
 maxStrikes = 5
 
-while((not hasUserWon(word, guesses)) and (strikes < maxStrikes)):
-    getLetter()
-    displaySecretWord(word, guesses)
-    hasUserWon(word, guesses)
+
+while((not hasUserWon(wordReveal, guesses)) and (strikes < maxStrikes)):
+  letter = getLetter()  
+  displaySecretWord(wordReveal, guesses)
+  # print(end="    ")
+  # print(guesses)
+  
+  if(letter in wordReveal):
+    print(end="    ")
+    print("Good choice!")
+          
+  else:
     strikes += 1
-    print(f" You have {strikes} strike. If you get 5, you lose!")
-    print("")
+    print(end="    ")
+    print(f"{letter} is not in the word.")
+    print()
+    print(f"You have {strikes} strike(s). If you get 5, you lose!")
+      
+  hasUserWon(wordReveal, guesses)      
+
+if(hasUserWon(wordReveal, guesses) and (strikes < maxStrikes)):
+  print()
+  print(Fore.GREEN + Style.BRIGHT + "Grats. You win.")
+  print(Style.RESET_ALL)
+else:
+  print()  
+  print(Fore.RED + Style.BRIGHT + "Game over. You lose.")
+  print(Style.RESET_ALL)
